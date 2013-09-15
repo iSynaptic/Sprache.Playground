16dbf2be33331a623c9930f2b80f2fb6bf097452
Jordan Terrell <jterrell@wans.net>
2013-09-15 08:48:02 -0500

    Created Postion struct...

---
+++ src/Sprache.Playground.UnitTests/PositionTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Sprache.Playground.UnitTests
+{
+    [TestFixture]
+    public class PositionTests
+    {
+        [Test]
+        public void DefaultValue_HasIndex0Line1Column1()
+        {
+            var pos = default(Position);
+
+            pos.Index.Should().Be(0);
+            pos.Line.Should().Be(1);
+            pos.Column.Should().Be(1);
+        }
+
+        [Test]
+        public void NegativeIndex_ThrowsException()
+        {
+            Action<int> act = index => new Position(index, 1, 1);
+
+            foreach (var index in Enumerable.Range(-10, 10))
+            {
+                int idx = index;
+                act.Invoking(a => a(idx)).ShouldThrow<ArgumentOutOfRangeException>();
+            }
+        }
+
+        [Test]
+        public void Line_WhenZeroOrLessThan_ThrowsException()
+        {
+            Action<int> act = line => new Position(0, line, 1);
+
+            foreach (var line in Enumerable.Range(-10, 11))
+            {
+                int l = line;
+                act.Invoking(a => a(l)).ShouldThrow<ArgumentOutOfRangeException>();
+            }
+        }
+
+        [Test]
+        public void Column_WhenZeroOrLessThan_ThrowsException()
+        {
+            Action<int> act = column => new Position(0, 1, column);
+
+            foreach (var column in Enumerable.Range(-10, 11))
+            {
+                int c = column;
+                act.Invoking(a => a(c)).ShouldThrow<ArgumentOutOfRangeException>();
+            }
+        }
+
+        [Test]
+        public void ProvidedValuesAreReturned()
+        {
+            var pos = new Position(45, 13, 4);
+
+            pos.Index.Should().Be(45);
+            pos.Line.Should().Be(13);
+            pos.Column.Should().Be(4);
+        }
+    }
+}
---
+++ src/Sprache.Playground/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sprache.Playground
+{
+    public struct Position
+    {
+        private readonly int _index;
+        private readonly int _line;
+        private readonly int _column;
+
+        public Position(int index, int line, int column)
+            : this()
+        {
+            if(index < 0) throw new ArgumentOutOfRangeException("index", "Index must not be negative.");
+            if (line <= 0) throw new ArgumentOutOfRangeException("line", "Line must be greater than or equal to one.");
+            if (column <= 0) throw new ArgumentOutOfRangeException("column", "Column must be greater than or equal to one.");
+
+            _index = index;
+            _line = line;
+            _column = column;
+        }
+
+        public int Index { get { return _index; } }
+
+        public int Line { get { return _line == 0 ? 1 : _line; } }
+        public int Column { get { return _column == 0 ? 1 : _column; } }
+    }
+}
